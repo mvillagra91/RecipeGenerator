e048c7689808ddc2778d016d89b4319a1c36ca9b
Mario Villagra <mvillagra.lorca@gmail.com>
2024-11-28 19:55:18 -0300

    Added Tailwind and Modification of archives

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import './styles/App.css'
 
+//Funcion App vendria siendo la pantalla principal (¿Recomendable llamar componentes aquí?)
 function App() {
-
-
   return (
-    <>
-      <div>Hola Mundo!!</div>
-    </>
+    <div>Hola mundo!!</div>
   )
 }
 
