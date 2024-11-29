2dfe9ae29a13157e1d5e8841405c3bcc1c6087f2
Mario Villagra <mvillagra.lorca@gmail.com>
2024-11-28 22:02:56 -0300

    Added Install Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import {Menu} from "./components/Menu"
 //Funcion App vendria siendo la pantalla principal (¿Recomendable llamar componentes aquí?)
 function App() {
   return (
-    <Menu/>
+    <>
+      <h1 className="text-3xl font-bold underline text-green-500">
+        Hello world!
+      </h1>
+      <Menu/>
+    </>
+    
   )
 }
 
