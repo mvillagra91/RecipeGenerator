e80dcc04a4c872146dab8aa32c1fe3a5ae10f847
Mario Villagra <mvillagra.lorca@gmail.com>
2024-11-30 02:40:09 -0300

    Added Dropdown

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,18 +2,26 @@
 
 export const Menu = () => {
   return (
-    <nav className="bg-black pt-3 pb-3">
-        <ul className="flex gap-5 pl-5 ">
-            <li>Home</li>
-            <li>Recetas</li>
-            <li>Busqueda de recetas por:
-              <ul className="hidden">
-                <li>Nombre</li>
-                <li>Ingredientes</li>
-                <li>Nacionalidad</li>
-              </ul>
-            </li>
+    <div>
+      <nav className="bg-black pt-1 pb-1 shadow-2xl font-bold">
+        <ul className="flex gap-5 justify-center">
+            <li className="cursor-pointer">Home</li>
+            <li className="cursor-pointer">Recetas</li>
+            <ul className="flex">
+              <div className="relative group">
+                  <div className="flex items-center">
+                      <li className="cursor-pointer">Buscar recetas por:</li>
+                  </div>
+                  <div className="absolute px-1 py-1 hidden group-hover:block bg-black shadow-lg rounded-b-lg">
+                    <li className="cursor-pointer pt-1 px-4 py-1 text-white hover:bg-white rounded hover:text-black">Nacionalidad</li>
+                    <li className="cursor-pointer pt-1 px-5 py-1 text-white hover:bg-white rounded hover:text-black">Ingredientes</li>
+                    <li className="cursor-pointer pt-1 px-8 py-1 text-white hover:bg-white rounded hover:text-black">Nombre</li>
+                  </div>
+              </div>
+          </ul>
         </ul>
-    </nav>
+      </nav>
+      
+    </div>
   )
 }
