183b647fa39bded93cc2acf0af6757678187461e
Mario Villagra <mvillagra.lorca@gmail.com>
2024-11-30 19:03:36 -0300

    Modifications not importants

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,12 +3,14 @@ import image from "../assets/images/1820.jpg"
 
 export const Body = () => {
     return (
-      <div className="flex justify-center min-h-screen">
-        <img
-          src={image}  // Ruta desde la raíz de public
-          alt="Descripción de la imagen"
-          className="w-full h-screen object-cover"
-        />
-      </div>
+      <>
+        <div className="flex justify-center min-h-screen">
+          <img
+            src={image}  // Ruta desde la raíz de public
+            alt="Descripción de la imagen"
+            className="w-full h-screen object-cover"
+          />
+        </div>
+      </>
     );
   };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,19 +10,21 @@ export const Menu = () => {
   ];
 
   return (
-    <div className="fixed flex justify-center w-full bg-transparent pt-3 shadow-2xl">
-      <nav className="flex max-w-md space-x-4 bg-gray-800 bg-opacity-90 rounded-xl">
-      {links.map(([title, url], index) => (
-        <a
-          key={index}
-          href={url}
-          className="rounded-lg px-3 py-2 text-white font-medium hover:bg-slate-100 hover:text-slate-900"
-          aria-current={window.location.pathname === url ? 'page' : undefined}
-        >
-          {title}
-        </a>
-      ))}
-      </nav>
-    </div>
+    <>
+      <div className="fixed flex justify-center w-full bg-transparent pt-3 shadow-2xl">
+        <nav className="flex max-w-md space-x-4 bg-gray-800 bg-opacity-90 rounded-xl">
+        {links.map(([title, url], index) => (
+          <a
+            key={index}
+            href={url}
+            className="rounded-lg px-3 py-2 text-white font-medium hover:bg-slate-100 hover:text-slate-900"
+            aria-current={window.location.pathname === url ? 'page' : undefined}
+          >
+            {title}
+          </a>
+        ))}
+        </nav>
+      </div>
+    </>
   );
 };
