70e4d252051ba390e4a075118e70c21c1f45296a
Mario Villagra <mvillagra.lorca@gmail.com>
2024-11-29 22:29:31 -0300

    Added Modification App and Menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import {Menu} from "./components/Menu"
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-green-500">
-        Hello world!
-      </h1>
       <Menu/>
     </>
   )
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,11 +2,17 @@
 
 export const Menu = () => {
   return (
-    <nav>
-        <ul>
+    <nav className="bg-black pt-3 pb-3">
+        <ul className="flex gap-5 pl-5 ">
             <li>Home</li>
-            <li>Recipes by ingredients</li>
-            <li>Search Recipes</li>
+            <li>Recetas</li>
+            <li>Busqueda de recetas por:
+              <ul className="hidden">
+                <li>Nombre</li>
+                <li>Ingredientes</li>
+                <li>Nacionalidad</li>
+              </ul>
+            </li>
         </ul>
     </nav>
   )
